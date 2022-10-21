7577d55543980b6bbb1b0c35a05026b2d6b55c93
jeeva1429 <116168359+jeeva1429@users.noreply.github.com>
2022-10-21 17:17:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const faceBackground = document.getElementById('face-background')
+const face = document.getElementById('face')
+
+faceBackground.oninput = () => {
+  face.style.fill = faceBackground.value
+}
+
+const textBackground = document.getElementById('text-background')
+const text = document.getElementById('text')
+
+textBackground.oninput = () => {
+  text.style.fill = textBackground.value
+}
+
+
+const beardBackground = document.getElementById('beard-background')
+const beard = document.getElementById('beard')
+
+beardBackground.oninput = () => {
+  beard.style.fill = beardBackground.value
+}
+
+
+const stripesBackground = document.getElementById('stripe-background')
+const stripes = document.getElementById('stripes')
+
+stripesBackground.oninput = () => {
+  stripes.style.fill = stripesBackground.value
+}
+
+
+const jerseyBackground = document.getElementById('jersey-background')
+const jersey = document.getElementById('Jersey')
+
+jerseyBackground.oninput = () => {
+  jersey.style.fill = jerseyBackground.value
+}
+
+const hairBackground = document.getElementById('hair-background')
+const hair = document.getElementById('hair')
+
+hairBackground.oninput = () => {
+  hair.style.fill = hairBackground.value
+}
+
+const background = document.getElementById('background')
+const Background = document.getElementById('Background')
+
+Background.oninput = () => {
+  background.style.fill = Background.value
+}
+
+
+
+
+
+
+
+
+
